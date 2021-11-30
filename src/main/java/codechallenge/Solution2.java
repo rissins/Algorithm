bdfb4f068974708188ddcb370bd6123fc9da82b3
rissins <tnalsqqq@naver.com>
2021-12-01 00:37:06 +0900

    음양 더하기

---
+++ src/main/java/codechallenge/Solution2.java
@@ -0,0 +1,16 @@
+package codechallenge;
+
+public class Solution2 {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int sum = 0;
+
+        for (int i = 0; i < absolutes.length; i++) {
+            if (signs[i]) {
+                sum += absolutes[i];
+            } else {
+                sum -= absolutes[i];
+            }
+        }
+        return sum;
+    }
+}
