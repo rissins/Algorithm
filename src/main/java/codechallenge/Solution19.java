c289f400a5b5d91b4655cf9d22386618bf84ea23
rissins <tnalsqqq@naver.com>
2022-01-05 18:33:25 +0900

    문자열을 정수로 바꾸기

---
+++ src/main/java/codechallenge/Solution19.java
@@ -0,0 +1,14 @@
+package codechallenge;
+
+//https://programmers.co.kr/learn/courses/30/lessons/12925
+public class Solution19 {
+    public int solution(String s) {
+        return Integer.parseInt(s);
+    }
+
+    public static void main(String[] args) {
+        Solution19 solution15 = new Solution19();
+        solution15.solution("-1234");
+
+    }
+}
