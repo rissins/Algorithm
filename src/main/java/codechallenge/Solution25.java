53fc57aa3a19bb620b3b04c5d6f67ac672b133eb
rissins <tnalsqqq@naver.com>
2022-01-13 23:26:52 +0900

    짝수와 홀수

---
+++ src/main/java/codechallenge/Solution25.java
@@ -0,0 +1,9 @@
+package codechallenge;
+
+
+//https://programmers.co.kr/learn/courses/30/lessons/12937
+public class Solution25 {
+    public String solution(int num) {
+        return num % 2 == 0 ? "Even": "Odd";
+    }
+}
