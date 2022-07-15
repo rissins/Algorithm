2f80b8bfe6cb4c20d9f22dbea3a0b06cba69b14d
rissins <tnalsqqq@naver.com>
2022-07-15 16:17:22 +0900

    위장

---
+++ src/main/java/algorithm/Solution8.java
@@ -0,0 +1,22 @@
+package algorithm;
+
+import java.util.*;
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/42578
+public class Solution8 {
+    public static int solution(String[][] clothes) {
+        int answer = 1;
+        HashMap<String, Integer> hash = new HashMap<>();
+
+        for(int i=0; i<clothes.length; i++){
+            String cloth_type = clothes[i][1];
+            hash.put(cloth_type, hash.getOrDefault(cloth_type, 1) + 1);
+        }
+
+        for (String key : hash.keySet()) {
+            answer *= hash.get(key);
+        }
+
+        return answer-1;
+    }
+}
