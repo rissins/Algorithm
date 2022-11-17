5738504aef6520abbe5b4069a3b7e4643b40bc8b
rissins <tnalsqqq@naver.com>
2022-11-17 22:43:46 +0900

    프로그래머스 132267

---
+++ src/main/java/programmers/P132267.java
@@ -0,0 +1,14 @@
+package programmers;
+
+public class P132267 {
+    public int solution(int a, int b, int n) {
+        int answer = 0;
+
+        while (n >= a) {
+            answer += (n / a) * b;
+            int remain = n % a;
+            n = (n / a) * b + remain;
+        }
+        return answer;
+    }
+}
